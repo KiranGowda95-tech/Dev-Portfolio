f3a595ecec8d1fd9a16ffa6b4f9c404f72d8b7d0
Kiran <kirankumardn74@gmail.com>
2024-08-08 11:16:20 +0530

    about image file updated

--- devPortfolio/src/components/About.jsx
+++ devPortfolio/src/components/About.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import SectionTitle from "./SectionTitle";
+import about from '../assets/about.jpg'
 
 const About = () => {
   return (
     <section className="bg-white py-20" id="about">
       <div className="align-element grid md:grid-cols-2 items-center gap-16">
-        <img src="" className="w-full h-16" />
+        <img src={about} className="w-full h-22" />
         <article>
           <SectionTitle text="code and coffee" />
           <p className="text-slate-600 mt-8 leading-loose">
